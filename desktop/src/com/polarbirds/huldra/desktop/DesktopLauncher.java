5709f1d7db529a75958737246dab84b08e3ca639
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-04-08 18:17:59 +0200

    Initial commit :cake:

---
+++ desktop/src/com/polarbirds/huldra/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.polarbirds.huldra.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.polarbirds.huldra.HuldraGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HuldraGame(), config);
+	}
+}
