8627531a7909fdbbec8465f96312af9476cbe9aa
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-05-11 14:40:41 +0200

    Generated world is now actually added in box2dWorld as Edge-fixture-bodies

--- core/src/com/polarbirds/huldra/screen/game/world/HuldraWorld.java
+++ core/src/com/polarbirds/huldra/screen/game/world/HuldraWorld.java
@@ -2,12 +2,19 @@ package com.polarbirds.huldra.screen.game.world;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.polarbirds.huldra.screen.game.world.inanimateObject.Interactable;
+import com.smokebox.lib.utils.geom.Line;
+import com.smokebox.lib.utils.geom.Rectangle;
 import com.smokebox.lib.utils.geom.UnifiablePolyedge;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
 
 /**
  * A class for this game's World. HuldraWorld contains a 2d array of Tile objects
@@ -25,7 +32,6 @@ public final class HuldraWorld {
     box2dWorld = new World(new Vector2(0, -9.81f), false);
 
     int[][] ints = new int[tiles.length][tiles[0].length];
-
     for(int x = 0; x < tiles.length; x++) {
       for(int y = 0; y < tiles[0].length; y++) {
         switch (tiles[x][y]) {
@@ -38,7 +44,22 @@ public final class HuldraWorld {
       }
     }
 
-    UnifiablePolyedge polyedge = new UnifiablePolyedge();
+    UnifiablePolyedge p = new UnifiablePolyedge(ints);
+    p.unify();
+
+    for(Line l : p.getEdges()) {
+      EdgeShape edgeShape = new EdgeShape();
+      edgeShape.set(l.x, l.y, l.x2, l.y2);
+
+      FixtureDef fixtureDef = new FixtureDef();
+      fixtureDef.shape = edgeShape;
+
+      BodyDef bodyDef = new BodyDef();
+      bodyDef.type = BodyDef.BodyType.StaticBody;
+      Body body = box2dWorld.createBody(bodyDef);
+
+      body.createFixture(fixtureDef);
+    }
   }
 
   public void step(float delta) {
@@ -66,7 +87,7 @@ public final class HuldraWorld {
 
     FOREST {
       @Override
-      public HuldraWorld getNew(String seed, OrthographicCamera camera) {
+      public HuldraWorld getNew(long seed, OrthographicCamera camera) {
         TileType[][] tiles = getEmptyTilesArray(16, 16);
         ArrayList<Interactable> interactables = new ArrayList<>();
 
@@ -78,8 +99,9 @@ public final class HuldraWorld {
 
     CAVES_DEEP {
       @Override
-      public HuldraWorld getNew(String seed, OrthographicCamera camera) {
+      public HuldraWorld getNew(long seed, OrthographicCamera camera) {
         TileType[][] tiles = getEmptyTilesArray(16, 16);
+        Random random = new Random(seed);
         ArrayList<Interactable> interactables = new ArrayList<>();
 
         // generate
@@ -90,7 +112,7 @@ public final class HuldraWorld {
 
     TEST_STAGE {
       @Override
-      public HuldraWorld getNew(String seed, OrthographicCamera camera) {
+      public HuldraWorld getNew(long seed, OrthographicCamera camera) {
         TileType[][] tiles = getEmptyTilesArray(16, 16);
         ArrayList<Interactable> interactables = new ArrayList<>();
 
@@ -101,7 +123,6 @@ public final class HuldraWorld {
             }
           }
         }
-        // generate
 
         return new HuldraWorld(tiles, interactables);
       }
@@ -109,7 +130,7 @@ public final class HuldraWorld {
 
     ;
 
-    public abstract HuldraWorld getNew(String seed, OrthographicCamera camera);
+    public abstract HuldraWorld getNew(long seed, OrthographicCamera camera);
   }
 
   private static TileType[][] getEmptyTilesArray(int width, int height) {
@@ -129,13 +150,19 @@ public final class HuldraWorld {
     LADDER
   }
 
-  private enum SectionType {
-    ALL,
+  private class Section {
+
+    TileType[][] tiles;
+
+    Section(Opening[] entries, Opening[] exits) {
+
+    }
+  }
+
+  private enum Opening {
     TOP,
     LEFT,
     RIGHT,
-    DOWN,
-    LEFT_RIGHT,
-    TOP_DOWN
+    BOTTOM
   }
 }
