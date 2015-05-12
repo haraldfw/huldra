801f34baf5cc28374337e727af12b9ff9d18c9b3
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-05-12 18:25:44 +0200

    Interactable is now an abstract object

--- core/src/com/polarbirds/huldra/model/entity/inanimateObject/Interactable.java
+++ core/src/com/polarbirds/huldra/model/entity/inanimateObject/Interactable.java
@@ -1,11 +1,22 @@
 package com.polarbirds.huldra.model.entity.inanimateobject;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.polarbirds.huldra.model.entity.BasicEntity;
+
 /**
  * An object than can be interacted with. Some by the player, and some
  * by other entities like enemies
  * Created by Harald on 30.4.15.
  */
-public interface Interactable {
+public class Interactable extends BasicEntity {
 
-  void interact();
+  public Interactable(Image image) {
+    super(image);
+
+  }
+
+  @Override
+  public void reset() {
+
+  }
 }
