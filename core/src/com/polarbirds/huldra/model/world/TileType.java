0e193a96e5de4114f7a524b0e750d9b7bc3161cc
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-05-14 12:16:22 +0200

    Added water as a tileType

--- core/src/com/polarbirds/huldra/model/world/TileType.java
+++ core/src/com/polarbirds/huldra/model/world/TileType.java
@@ -1,11 +1,13 @@
 package com.polarbirds.huldra.model.world;
 
 /**
+ * Enum for all available TileTypes
  * Created by Harald Wilhelmsen on 13/5/2015.
  */
 enum TileType {
   EMPTY,
   SOLID,
   PLATFORM,
-  LADDER
+  LADDER,
+  WATER
 }
