f14a93d582a902261589d19c830e156d93e146ff
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-05-16 12:57:46 +0200

    Added a simple section TMX and a tilemap

---
+++ core/assets/tiles/ground.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ground" tilewidth="16" tileheight="16">
+ <tile id="0">
+  <properties>
+   <property name="entries" value="LEFT RIGHT UP"/>
+   <property name="exits" value="LEFT RIGHT"/>
+  </properties>
+  <image width="16" height="16" source="ground.png"/>
+ </tile>
+</tileset>
