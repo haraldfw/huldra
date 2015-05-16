4572246e8413708cbeee841bef557cba7dc88c21
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-05-16 20:17:47 +0200

    Section is now completely split between new Section and SectionBounds

--- core/src/com/polarbirds/huldra/model/world/Section.java
+++ core/src/com/polarbirds/huldra/model/world/Section.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
  */
 public class Section {
 
+  public static final int TILES_PER_SIDE = 8;
+
   SectionBounds bounds;
 
   /*
@@ -24,5 +26,45 @@ public class Section {
     openings.put(Side.RIGHT, new boolean[TILES_PER_SIDE * bounds.width]);
     openings.put(Side.BOTTOM, new boolean[TILES_PER_SIDE * bounds.height]);
     openings.put(Side.TOP, new boolean[TILES_PER_SIDE * bounds.height]);
+  }
+
+  /**
+   * Changes the given coordinate of the given side to the given boolean.
+   * @param side            The side to set.
+   * @param tileCoordinate  The coordinate for the tile to change.
+   * @param open            The boolean to set the tile to.
+   */
+  void setTileOpen(Side side, int tileCoordinate, boolean open) {
+    openings.get(side)[tileCoordinate] = open;
+  }
+
+  boolean[] getSideArray(Side side) {
+    return openings.get(side);
+  }
+
+  void setSide(Side side, boolean[] sideArray) {
+    int thisSideLength = openings.get(side).length;
+    if(thisSideLength != sideArray.length) {
+      System.out.println("Tried to set side '" + side + "' with an array with incorrect length!"
+                         + "\nLength was '" + sideArray
+                         + "' but should have been '" + thisSideLength + "'");
+      return;
+    }
+
+    System.arraycopy(sideArray, 0, openings.get(side), 0, sideArray.length);
+  }
+
+  static TileType[][] getTiles() {
+    TileType[][] tiles = new TileType[][]{
+        {TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID},
+        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
+        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
+        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
+        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
+        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
+        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
+        {TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID}
+    };
+    return tiles;
   }
 }
--- core/src/com/polarbirds/huldra/model/world/SectionBounds.java
+++ core/src/com/polarbirds/huldra/model/world/SectionBounds.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
  */
 final class SectionBounds {
 
-  public static final int TILES_PER_SIDE = 8;
   public static final int MAX_HEIGHT = 3;
   public static final int MAX_WIDTH = 3;
 
@@ -26,51 +25,11 @@ final class SectionBounds {
   }
 
   /**
-   * Changes the given coordinate of the given side to the given boolean.
-   * @param side            The side to set.
-   * @param tileCoordinate  The coordinate for the tile to change.
-   * @param open            The boolean to set the tile to.
-   */
-  void setTileOpen(Side side, int tileCoordinate, boolean open) {
-    openings.get(side)[tileCoordinate] = open;
-  }
-
-  /**
    * Returns a boolean that signifies whether the coordinates given overlap this SectionBounds.
    * @param v An IntVector2 containing the coordinates to check for.
    * @return  True if the coordinates overlap this section.
    */
   boolean contains(IntVector2 v) {
     return !(v.x < this.x || v.x > this.x + width || v.y < this.y || v.y > this.y + height);
-  }
-
-  boolean[] getSideArray(Side side) {
-    return openings.get(side);
-  }
-
-  void setSide(Side side, boolean[] sideArray) {
-    int thisSideLength = openings.get(side).length;
-    if(thisSideLength != sideArray.length) {
-      System.out.println("Tried to set side '" + side + "' with an array with incorrect length!"
-                         + "\nLength was '" + sideArray
-                         + "' but should have been '" + thisSideLength + "'");
-      return;
-    }
-
-    System.arraycopy(sideArray, 0, openings.get(side), 0, sideArray.length);
-  }
-
-  static TileType[][] getTiles() {
-    TileType[][] tiles = new TileType[][]{
-        {TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID},
-        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
-        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
-        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
-        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
-        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
-        {TileType.SOLID, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.EMPTY, TileType.SOLID},
-        {TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID, TileType.SOLID}
-    };
-    return tiles;
   }
 }
