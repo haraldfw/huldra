b25bbdb9614f5a7b0b29d5eb0f8d8740358200dd
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-05-23 13:13:29 +0200

    Fixed print and made constructor package private in Section

--- core/src/com/polarbirds/huldra/model/world/Section.java
+++ core/src/com/polarbirds/huldra/model/world/Section.java
@@ -17,7 +17,7 @@ public class Section {
   */
   private HashMap<Side, boolean[]> reachableOpenings;
 
-  public Section(SectionBounds bounds) {
+  Section(SectionBounds bounds) {
     this.bounds = bounds;
 
     reachableOpenings = new HashMap<>();
@@ -68,7 +68,7 @@ public class Section {
   void setSide(Side side, boolean[] sideArray) {
     int thisSideLength = reachableOpenings.get(side).length;
     if (thisSideLength != sideArray.length) {
-      System.out.println("Tried to set side '" + side + "' with an array with incorrect length!"
+      System.out.println("Tried to set side '" + side + "' with an array incorrect length!"
                          + "\nLength was '" + sideArray.length
                          + "' but should have been '" + thisSideLength + "'");
       return;
