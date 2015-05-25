6dce5e3f114a1643659bc3623b6160cf55c1da99
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-05-25 20:07:47 +0200

    Removed unnecessary methods

--- core/src/com/polarbirds/huldra/model/world/Section.java
+++ core/src/com/polarbirds/huldra/model/world/Section.java
@@ -39,30 +39,6 @@ public class Section {
     return booleans;
   }
 
-  /**
-   * Changes the given coordinate of the given side to the given boolean.
-   *
-   * @param side           The side to set.
-   * @param tileCoordinate The coordinate for the tile to change.
-   * @param open           The boolean to set the tile to.
-   */
-  void setTileOpen(Side side, int tileCoordinate, boolean open) {
-    reachableOpenings.get(side)[tileCoordinate] = open;
-  }
+  
 
-  boolean[] getSideArray(Side side) {
-    return reachableOpenings.get(side);
-  }
-
-  void setSide(Side side, boolean[] sideArray) {
-    int thisSideLength = reachableOpenings.get(side).length;
-    if (thisSideLength != sideArray.length) {
-      System.out.println("Tried to set side '" + side + "' with an array incorrect length!"
-                         + "\nLength was '" + sideArray.length
-                         + "' but should have been '" + thisSideLength + "'");
-      return;
-    }
-
-    System.arraycopy(sideArray, 0, reachableOpenings.get(side), 0, sideArray.length);
-  }
 }
