556017e11652ded043907d089df7023699040143
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-06-08 18:10:37 +0200

    Fixed SpreadComparator. Now compares absolutes.

--- core/src/com/polarbirds/huldra/model/world/SpreadComparator.java
+++ core/src/com/polarbirds/huldra/model/world/SpreadComparator.java
@@ -20,7 +20,7 @@ class SpreadComparator implements Comparator<Bounds> {
 
   @Override
   public int compare(Bounds b1, Bounds b2) {
-    return (b1.x + b1.width / 2f)*csHor + (b1.y + b1.height / 2f)*csVer <
-           (b2.x + b2.width / 2f)*csHor + (b2.y + b2.height / 2f)*csVer ? -1 : 1;
+    return Math.abs((b1.x + b1.width / 2f)*csHor + (b1.y + b1.height / 2f)*csVer) <
+           Math.abs((b2.x + b2.width / 2f)*csHor + (b2.y + b2.height / 2f)*csVer) ? -1 : 1;
   }
 }
