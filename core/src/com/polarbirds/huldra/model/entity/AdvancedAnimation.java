d787496a4286132d5fd1f9e8510ab84c74afbe5e
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-06-16 04:57:01 +0200

    Added classes for animations with single and multiple frametimes

---
+++ core/src/com/polarbirds/huldra/model/entity/AdvancedAnimation.java
@@ -0,0 +1,39 @@
+package com.polarbirds.huldra.model.entity;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * A class for animations with different frametimes per frame.
+ * Created by Harald Wilhelmsen on 16/6/2015.
+ */
+public class AdvancedAnimation extends AAnimation {
+
+  private final Sprite[] frames;
+  private final float[] frameTimes;
+
+  public AdvancedAnimation(Sprite[] frames, float[] frameTimes) {
+    this.frames = frames;
+    this.frameTimes = frameTimes;
+  }
+
+  @Override
+  protected float getTotalTime() {
+    float totalTime = 0;
+    for (float f : frameTimes) {
+      totalTime += f;
+    }
+    return totalTime;
+  }
+
+  @Override
+  protected Sprite getCurrentFrame() {
+    float t = 0;
+    for (int i = 0; i < frames.length; i++) {
+      t += frameTimes[i];
+      if (t > timePassed) {
+        return frames[i];
+      }
+    }
+    return frames[0];
+  }
+}
---
+++ core/src/com/polarbirds/huldra/model/entity/SimpleAnimation.java
@@ -0,0 +1,28 @@
+package com.polarbirds.huldra.model.entity;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * A class for animations with equal framtimes for each frame.
+ * Created by Harald Wilhelmsen on 16/6/2015.
+ */
+public class SimpleAnimation extends AAnimation {
+
+  private final Sprite[] frames;
+  private final float timePerFrame;
+
+  SimpleAnimation(Sprite[] frames, float timePerFrame) {
+    this.frames = frames;
+    this.timePerFrame = timePerFrame;
+  }
+
+  @Override
+  protected float getTotalTime() {
+    return timePerFrame * frames.length;
+  }
+
+  @Override
+  protected Sprite getCurrentFrame() {
+    return frames[(int) (timePassed / timePerFrame)];
+  }
+}
