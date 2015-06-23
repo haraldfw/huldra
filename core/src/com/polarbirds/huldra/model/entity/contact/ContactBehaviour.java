f11ab73e0c478a32e9feec9be193b5f207201f92
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-06-23 10:58:19 +0200

    Created a class for a custom ContactListener

---
+++ core/src/com/polarbirds/huldra/model/entity/contact/ContactBehaviour.java
@@ -0,0 +1,32 @@
+package com.polarbirds.huldra.model.entity.contact;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+/**
+ * Created by Harald Wilhelmsen on 16/6/2015.
+ */
+public class ContactBehaviour implements ContactListener {
+
+  @Override
+  public void preSolve(Contact contact, Manifold oldManifold) {
+
+  }
+
+  @Override
+  public void beginContact(Contact contact) {
+
+  }
+
+  @Override
+  public void endContact(Contact contact) {
+
+  }
+
+  @Override
+  public void postSolve(Contact contact, ContactImpulse impulse) {
+
+  }
+}
