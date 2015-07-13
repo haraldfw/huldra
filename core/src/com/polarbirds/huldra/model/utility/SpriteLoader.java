8656a37bc5fc4cf768012e8d05c7f5e22ffd7e6d
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-07-13 13:46:19 +0200

    Started working on loading animations

--- core/src/com/polarbirds/huldra/model/utility/SpriteLoader.java
+++ core/src/com/polarbirds/huldra/model/utility/SpriteLoader.java
@@ -1,9 +1,11 @@
 package com.polarbirds.huldra.model.utility;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.polarbirds.huldra.model.entity.animation.AAnimation;
 import com.polarbirds.huldra.model.world.physics.Vector2;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -19,6 +21,7 @@ public class SpriteLoader extends ALoader {
   private double progress = 0;
   private Collection<String> paths = new ArrayList<>();
   private Map<String, Sprite> loadedSprites;
+  private Map<String, AAnimation> loadedAnimations;
 
   @Override
   public void run() {
@@ -26,8 +29,21 @@ public class SpriteLoader extends ALoader {
 
     double progressIncrement = 1.0 / paths.size();
     for (String path : paths) {
-      if (!loadedSprites.containsKey(path)) {
-        loadedSprites.put(path, new Sprite(new Texture(path), parseGraphicsDescriptor(path)));
+      if (path.contains(".anim")) {
+        Texture texture = new Texture(path.substring(0, path.length() - 5) + ".png");
+        int i = 0;
+        while (true) {
+          File file = new File(
+              path.substring(path.lastIndexOf("\\"), path.length() - 5) + String.valueOf(i));
+          if (file.isFile() && file.canWrite() && file.length() > 0) {
+            // File exists
+
+          } else {
+            break;
+          }
+        }
+      } else {
+        loadSprite(path);
       }
     }
 
@@ -35,12 +51,19 @@ public class SpriteLoader extends ALoader {
     isDone = true;
   }
 
+  private void loadSprite(String path) {
+    if (!loadedSprites.containsKey(path)) {
+      loadedSprites.put(path, new Sprite(new Texture(path), parseGraphicsDescriptor(path)));
+    }
+  }
+
+
   public void queueAsset(String toAdd) {
     paths.add(toAdd);
   }
 
   public void queueAssets(String[] toAdd) {
-    for(String path : paths) {
+    for (String path : paths) {
       paths.add(path);
     }
   }
@@ -49,13 +72,18 @@ public class SpriteLoader extends ALoader {
     return loadedSprites.get(path);
   }
 
+  public AAnimation getAnimation(String path) {
+    return loadedAnimations.get(path);
+  }
+
   /**
    * Returns progress in parts per one.
+   *
    * @return Progress as a double
    */
   @Override
   public double getProgress() {
-    double progress = loadedSprites.size() / paths.size();
+    double progress = (double) loadedSprites.size() / paths.size();
     System.out.println(
         "Texture-loading progress: " + loadedSprites.size() + "/" + paths.size() + ", "
         + progress * 100 + "%");
@@ -71,6 +99,21 @@ public class SpriteLoader extends ALoader {
       System.out.println("No descriptor for png '" + path + "' found. Using shift [0, 0]");
     }
     return new Vector2();
+  }
+
+  private enum loadEnum {
+    KNIGHT(new String[]{"graphics/player/knight/walk.anim",
+                        "graphics/player/knight/idle.anim",
+                        "graphics/player/knight/slash.anim",
+                        "graphics/player/knight/dance.anim",
+                        "graphics/player/knight/jump.anim"}
+    ),;
+
+    public final String[] loadString;
+
+    loadEnum(String[] loadString) {
+      this.loadString = loadString;
+    }
   }
 
 }
