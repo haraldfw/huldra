9edace5db7da88154e7fef7a7943bbd1b61b8aed
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-07-23 13:22:18 +0200

    Moved level-conversion to BoundGenerator. Moved Level-class

--- core/src/com/polarbirds/huldra/model/world/BoundGenerator.java
+++ core/src/com/polarbirds/huldra/model/world/BoundGenerator.java
@@ -1,9 +1,12 @@
 package com.polarbirds.huldra.model.world;
 
+import com.polarbirds.huldra.model.world.physics.Vector2;
 import com.smokebox.lib.utils.IntVector2;
 import com.smokebox.lib.utils.geom.Bounds;
+import com.smokebox.lib.utils.geom.Line;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Random;
@@ -100,7 +103,7 @@ final class BoundGenerator {
         return value < min ? min : value > max ? max : value;
     }
 
-    List<Bounds> generateBoundsList(int amountOfSections) {
+    private List<Bounds> generateBoundsList(int amountOfSections) {
         ArrayList<Bounds> boundsList = new ArrayList<>();
 
         boundsList.add(new Bounds(0, 0, 1, 1));
@@ -146,5 +149,240 @@ final class BoundGenerator {
             }
         }
         return boundsList;
+    }
+
+    public TileArrayWithSpawn asTiles(int sectionAmount, Random random) {
+        Iterable<Bounds> boundsList = generateBoundsList(sectionAmount);
+        List<TilesWithOpenings> twos = TilesWithOpenings.loadAndGetList();
+
+        // normalize bounds. Since the spawn was previously on 0,0 it is now located on the shift
+        // that was applied
+        IntVector2 intSpawn = normalizeBoundsList(boundsList);
+
+        Collection<Section> sections = new ArrayList<>();
+        for (Bounds bounds : boundsList) {
+            sections.add(new Section(bounds));
+        }
+
+        IntVector2 maxBounds = getMaxBounds(boundsList);
+
+        // Array for map-tiles
+        TileType[][] tiles = new TileType
+                [maxBounds.x * Section.TILES_PER_SIDE + 2][maxBounds.y * Section.TILES_PER_SIDE + 2];
+
+        for (TileType[] tileTypes : tiles) {
+            Arrays.fill(tileTypes, TileType.SOLID);
+        }
+
+        // Array of reachable openings
+        boolean[][] reachableOpenings =
+                addSectionOpenings(sections, new boolean[tiles.length][tiles[0].length]);
+
+        // Add the section's tiles to the map
+        for (Section section : sections) {
+            TilesWithOpenings two = getTilesForSection(section, reachableOpenings, twos, random);
+            TileType[][] sectionTiles = two.tiles;
+            section.tilesWithOpenings = two;
+            int baseX = section.bounds.x * Section.TILES_PER_SIDE;
+            int baseY = section.bounds.y * Section.TILES_PER_SIDE;
+            for (int x = 0; x < sectionTiles.length; x++) {
+                System.arraycopy(sectionTiles[x], 0, tiles[baseX + x + 1], baseY + 1,
+                        sectionTiles[0].length);
+            }
+        }
+
+        // add an opening in all tiles in each bounds
+        for (Bounds bounds : boundsList) {
+            for (int x = 0; x < bounds.width * Section.TILES_PER_SIDE; x++) {
+                for (int y = 0; y < bounds.height * Section.TILES_PER_SIDE; y++) {
+                    reachableOpenings[bounds.x * Section.TILES_PER_SIDE + x]
+                            [bounds.y * Section.TILES_PER_SIDE + y] = true;
+                }
+            }
+        }
+
+        Section spawnSection = null;
+        for (Section s : sections) {
+            if (intSpawn.isLike(-s.bounds.x, -s.bounds.y)) {
+                spawnSection = s;
+            }
+        }
+        List<IntVector2> spawns = spawnSection.tilesWithOpenings.locs.get("SPAWN");
+        IntVector2 gottenSpawn = spawns.get(spawns.size() - 1);
+        Vector2 spawn = new Vector2(spawnSection.bounds.x * Section.TILES_PER_SIDE + gottenSpawn.x + 1,
+                spawnSection.bounds.y * Section.TILES_PER_SIDE + gottenSpawn.y + 1);
+        return new TileArrayWithSpawn(tiles, spawn);
+    }
+
+
+    /**
+     * Returns tiles for the given sectionBounds, taking into account the sectionBounds' openings
+     */
+    private TilesWithOpenings getTilesForSection(Section section, boolean[][] openings,
+                                                 Iterable<TilesWithOpenings> twos, Random random) {
+        List<TilesWithOpenings> candidates = new ArrayList<>();
+        for (TilesWithOpenings two : twos) {
+            if (two.matches(section.bounds, openings)) {
+                candidates.add(two);
+            }
+            TilesWithOpenings twoFlipped = two.getFlipped();
+            if (twoFlipped.matches(section.bounds, openings)) {
+                candidates.add(twoFlipped);
+            }
+        }
+        return candidates.get(random.nextInt(candidates.size()));
+    }
+
+    private boolean allTrue(boolean[] bs) {
+        for (boolean b : bs) {
+            if (!b) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private TileType[][] placeholderTiles(Section section) {
+        TileType[][] tiles =
+                new TileType[section.bounds.width * Section.TILES_PER_SIDE][section.bounds.height
+                        * Section.TILES_PER_SIDE];
+        for (int x = 0; x < tiles.length; x++) {
+            for (int y = 0; y < tiles[0].length; y++) {
+                tiles[x][y] = x == 0 || y == 0 || x == tiles.length - 1 || y == tiles[0].length - 1
+                        ? TileType.SOLID : TileType.EMPTY;
+            }
+        }
+        return tiles;
+    }
+
+    private boolean[][] addSectionOpenings(Iterable<Section> sections, boolean[][] openings) {
+        // create an array to keep track of where there are sections
+        boolean[][] fills = new boolean[openings.length][openings[0].length];
+        for (Section section : sections) {
+            Bounds b = section.bounds;
+            for (int x = b.x * Section.TILES_PER_SIDE; x < (b.x + b.width) * Section.TILES_PER_SIDE;
+                 x++) {
+                for (int y = b.y * Section.TILES_PER_SIDE; y < (b.y + b.height) * Section.TILES_PER_SIDE;
+                     y++) {
+                    fills[x][y] = true;
+                }
+            }
+        }
+
+        // Make sure boundaries of sections are true in reachableOpenings-array
+        for (Section section : sections) {
+            Bounds b = section.bounds;
+            for (int y = b.y * Section.TILES_PER_SIDE; y < (b.y + b.height) * Section.TILES_PER_SIDE;
+                 y++) {
+                int xCheck = b.x * Section.TILES_PER_SIDE - 1;
+                if (xCheck >= 0 && fills[xCheck][y]) {
+                    openings[xCheck + 1][y] = true;
+                }
+                xCheck = (b.x + b.width) * Section.TILES_PER_SIDE;
+                if (xCheck < fills.length && fills[(b.x + b.width) * Section.TILES_PER_SIDE][y]) {
+                    openings[xCheck - 1][y] = true;
+                }
+            }
+            for (int x = b.x * Section.TILES_PER_SIDE; x < (b.x + b.width) * Section.TILES_PER_SIDE;
+                 x++) {
+                int yCheck = b.y * Section.TILES_PER_SIDE - 1;
+                if (yCheck >= 0 && fills[x][yCheck]) {
+                    openings[x][yCheck + 1] = true;
+                }
+                yCheck = (b.y + b.height) * Section.TILES_PER_SIDE;
+                if (yCheck < fills[0].length && fills[x][yCheck]) {
+                    openings[x][yCheck - 1] = true;
+                }
+            }
+        }
+        return openings;
+    }
+
+    private void placeVerTrue(int startX, int startY, int length, boolean[][] openings) {
+        for (int y = 0; y < length; y++) {
+            openings[startX][startY + y] = true;
+        }
+    }
+
+    private void placeHorTrue(int startX, int startY, int length, boolean[][] openings) {
+        for (int x = 0; x < length; x++) {
+            openings[startX + x][startY] = true;
+        }
+    }
+
+    private int[][] getInts(TileType[][] mapTiles, TileType checkFor) {
+        int[][] ints = new int[mapTiles.length][mapTiles[0].length];
+        for (int x = 0; x < mapTiles.length; x++) {
+            for (int y = 0; y < mapTiles[0].length; y++) {
+                TileType tile = mapTiles[x][y];
+                if (tile == checkFor) {
+                    ints[x][y] = 1;
+                }
+            }
+        }
+        return ints;
+    }
+
+    /**
+     * Normalizes the given list of bounds. What this means is that it makes sure no bounds have
+     * negative coordinates, and the lowest coordinates will be (0, 0)
+     *
+     * @param boundsList List of bounds to normalize
+     * @return IntVector of the shift that was applied
+     */
+    private IntVector2 normalizeBoundsList(Iterable<Bounds> boundsList) {
+        IntVector2 shift = new IntVector2(Integer.MAX_VALUE, Integer.MAX_VALUE);
+
+        for (Bounds bounds : boundsList) {
+            if (bounds.x < shift.x) {
+                shift.x = bounds.x;
+            }
+            if (bounds.y < shift.y) {
+                shift.y = bounds.y;
+            }
+        }
+
+        for (Bounds bounds : boundsList) {
+            bounds.x -= shift.x;
+            bounds.y -= shift.y;
+        }
+
+        return shift;
+    }
+
+    /**
+     * Returns the max bounds of the given list.
+     *
+     * @param boundsList A normalized list of bounds-objects
+     * @return The max bounds as an IntVector2
+     */
+    private IntVector2 getMaxBounds(Iterable<Bounds> boundsList) {
+        IntVector2 max = new IntVector2(Integer.MIN_VALUE, Integer.MIN_VALUE);
+
+        for (Bounds bounds : boundsList) {
+
+            if (bounds.x + bounds.width > max.x) {
+                max.x = bounds.x + bounds.width;
+            }
+
+            if (bounds.y + bounds.height > max.y) {
+                max.y = bounds.y + bounds.height;
+            }
+
+        }
+        return max;
+    }
+
+    private ArrayList<Line> getPlatforms(int[][] ints) {
+        ArrayList<Line> lines = new ArrayList<>();
+        for (int x = 0; x < ints.length; x++) {
+            for (int y = 0; y < ints[0].length; y++) {
+                if (ints[x][y] == 1) {
+                    float newY = y + 1;
+                    lines.add(new Line(x, newY, x + 1, newY));
+                }
+            }
+        }
+        return lines;
     }
 }
--- core/src/com/polarbirds/huldra/model/world/Level.java
+++ core/src/com/polarbirds/huldra/model/world/Level.java
@@ -1,28 +1,42 @@
-package com.polarbirds.huldra.screen.game;
+package com.polarbirds.huldra.model.world;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.polarbirds.huldra.model.entity.character.player.PlayerCharacter;
-import com.polarbirds.huldra.model.world.HuldraWorld;
-import com.polarbirds.huldra.model.world.WorldType;
 import com.polarbirds.huldra.model.world.physics.Vector2;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 /**
  * Created by Harald on 19.07.2015.
  */
 public class Level {
-    public Vector2 spawn;
-    ArrayList<PlayerCharacter> players;
-    private HuldraWorld world;
 
-    public Level(ArrayList<PlayerCharacter> players, int level, Random random) {
+    public final int difficulty;
+    public TileType[][] tiles;
+    public Vector2 spawn;
+    public PlayerCharacter[] players;
+    private HashMap<TileType, Texture> tileTextures;
+
+    public Level(PlayerCharacter[] players, Random random) {
         this.players = players;
-        parseLevel(level, random);
+        difficulty = 1; // 1 is starting difficulty, increments by 1 for each new
+        parseLevel(difficulty, random);
+    }
+
+    public Level(Level lastLevel, Random random) {
+        players = lastLevel.players;
+        difficulty = lastLevel.difficulty + 1;
+        parseLevel(difficulty, random);
+    }
+
+    public void draw(Batch batch) {
+
     }
 
     private void parseLevel(int level, Random random) {
@@ -42,8 +56,10 @@ public class Level {
                     type = WorldType.TEST_STAGE;
             }
             int sectionAmount = Integer.parseInt(reader.readLine());
-
-            world = new HuldraWorld(sectionAmount, type, random);
+            TileArrayWithSpawn tileArrayWithSpawn =
+                    new BoundGenerator(type, random).asTiles(sectionAmount, random);
+            tiles = tileArrayWithSpawn.tiles;
+            spawn = tileArrayWithSpawn.spawn;
         } catch (IOException e) {
             e.printStackTrace();
         }
