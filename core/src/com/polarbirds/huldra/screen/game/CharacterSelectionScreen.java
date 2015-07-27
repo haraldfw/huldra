b63d212393444fa7fcf6fa4e81c77053ebdd6697
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-07-27 15:11:07 +0200

    Belongs to last commit

--- core/src/com/polarbirds/huldra/screen/game/CharacterSelectionScreen.java
+++ core/src/com/polarbirds/huldra/screen/game/CharacterSelectionScreen.java
@@ -2,8 +2,12 @@ package com.polarbirds.huldra.screen.game;
 
 import com.badlogic.gdx.Screen;
 import com.polarbirds.huldra.HuldraGame;
+import com.polarbirds.huldra.model.character.Team;
+import com.polarbirds.huldra.model.character.player.Knight;
 import com.polarbirds.huldra.model.character.player.PlayerCharacter;
 import com.polarbirds.huldra.screen.loading.INeedsLoading;
+
+import java.util.ArrayList;
 
 /**
  * Created by Harald on 23.07.2015.
@@ -12,20 +16,23 @@ public class CharacterSelectionScreen implements Screen, INeedsLoading {
 
     private HuldraGame game;
 
-    private PlayerCharacter[] players;
+    private ArrayList<PlayerCharacter> playerList;
 
     public CharacterSelectionScreen(HuldraGame game) {
         this.game = game;
+        playerList = new ArrayList<>();
+        playerList.add(new Knight(Team.PLAYER));
     }
 
     @Override
     public void render(float delta) {
-
+        nextScreen();
     }
 
     @Override
     public void nextScreen() {
-        game.setScreen(new GameScreen(game, players));
+        game.setScreen(new GameScreen(
+            game, playerList.toArray(new PlayerCharacter[playerList.size()])));
     }
 
     @Override
