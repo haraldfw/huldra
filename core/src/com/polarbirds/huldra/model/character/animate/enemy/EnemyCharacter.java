9e0c857dcbb4a94b23c0320936b861a8ffa4626d
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-07-31 11:26:06 +0200

    Added some missing methods to EnemyCharacter
    This class should not have been committed yet at all. Whoops

--- core/src/com/polarbirds/huldra/model/character/animate/enemy/EnemyCharacter.java
+++ core/src/com/polarbirds/huldra/model/character/animate/enemy/EnemyCharacter.java
@@ -1,11 +1,58 @@
 package com.polarbirds.huldra.model.character.animate.enemy;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.polarbirds.huldra.model.character.Team;
 import com.polarbirds.huldra.model.character.animate.AWalkingCharacter;
+import com.polarbirds.huldra.model.character.animate.player.gear.GearHandler;
+import com.polarbirds.huldra.model.character.stat.StatModifier;
+import com.polarbirds.huldra.model.drawing.AAnimation;
+import com.polarbirds.huldra.model.drawing.singleframe.ASprite;
+import com.polarbirds.huldra.model.utility.SpriteLoader;
+
+import java.util.Map;
 
 /**
  * Created by Harald Wilhelmsen on 30/7/2015.
  */
 public class EnemyCharacter extends AWalkingCharacter {
 
+  public EnemyCharacter(float width, float height, float inverseMass, Team team) {
+    super(width, height, inverseMass, team);
+  }
 
+  @Override
+  public void update(float delta) {
+    super.update(delta);
+
+  }
+
+  @Override
+  public void draw(Batch batch, float parentAlpha) {
+    super.draw(batch, parentAlpha);
+  }
+
+  @Override
+  protected AAnimation getCurrentAnimation() {
+    return null;
+  }
+
+  @Override
+  public void initGraphics(Map<String, ASprite> sprites, Map<String, AAnimation> animations) {
+
+  }
+
+  @Override
+  public void queueAssets(SpriteLoader spriteLoader) {
+
+  }
+
+  @Override
+  public GearHandler getGear() {
+    return null;
+  }
+
+  @Override
+  public StatModifier[] getBaseStats() {
+    return new StatModifier[0];
+  }
 }
